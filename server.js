0ea29acb100f20764c4549992290033b52c14afb
Birgit <birgit.noormagi@tktk.ee>
2024-04-07 12:30:42 +0300

    add week1

---
+++ server.js
@@ -0,0 +1,15 @@
+//console.log("Hello World!");
+
+let firstName = "Harry";
+let lastName = "Potter";
+
+//console.log(firstName);
+
+//console.log("Hello, " + firstName + "!");
+
+//template literals
+
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
